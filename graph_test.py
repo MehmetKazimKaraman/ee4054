4e3eaf97e3c5895f178afd808bb9f7e621d798cf
MehmetKazimKaraman <90840696+MehmetKazimKaraman@users.noreply.github.com>
2021-11-02 21:01:46 +0300

    Update graph_test.py

--- graph_test.py
+++ graph_test.py
@@ -11,6 +11,9 @@ class Canvas(FigureCanvas):
         fig, self.ax = plt.subplots(figsize=(12, 8), dpi=100)
         super().__init__(fig)
         self.setParent(parent)
+        
+        
+        
 
         self.canvas = FigureCanvas(self.figure)
         self.toolbar = NavigationToolbar(self.canvas, self)
